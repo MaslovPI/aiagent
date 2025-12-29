51881c44c47e7f025fc0bc56f8703ce54e0e1e8e
Pavel Maslov <dnfanl@gmail.com>
2025-12-29 13:06:51 +0100

    Added console parametrs support

--- main.py
+++ main.py
@@ -1,19 +1,40 @@
 import os
+import argparse
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 
 def main():
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+
     load_dotenv()
+
     api_key = os.environ.get("GEMINI_API_KEY")
     if api_key is None:
         raise RuntimeError("Api key for AI provider is not found")
-
     client = genai.Client(api_key=api_key)
+
+    args = parser.parse_args()
+    prompt = args.user_prompt
+
+    messages = [types.Content(role="user", parts=[types.Part(text=prompt)])]
     response = client.models.generate_content(
         model="gemini-2.5-flash",
-        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+        contents=messages,
     )
+    if response.usage_metadata is None:
+        raise RuntimeError("API request has failed, metadata is empty")
+
+    tokens_prompt = response.usage_metadata.prompt_token_count
+    tokens_response = response.usage_metadata.candidates_token_count
+    if args.verbose:
+        print(f"User prompt: {prompt}")
+        print(f"Prompt tokens: {tokens_prompt}")
+        print(f"Response tokens: {tokens_response}")
+    print("Response:")
     print(response.text)
 
 
